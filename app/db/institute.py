e5aad020948148650e16a502252b0ed688e58665
Bernardo <bsoares.it@gmail.com>
2025-02-02 07:17:56 +0000

    methods to add entries to crypto and institute. still wip
    Signed-off-by: Bernardo <bsoares.it@gmail.com>

--- app/db/institute.py
+++ app/db/institute.py
@@ -18,3 +18,20 @@ class InstituteDatabase(DBClass):
         )
         """)
         self.conn.commit()
+
+    def delete(stlf, cik: str):
+        self.cursor.execute("DELETE FROM institutes WHERE cik = ?", (cik,))
+        self.conn.commit()
+
+    def add(stlf, cik, name: str):
+        self.cursor.execute("""
+            INSERT OR IGNORE INTO institutes (cik, name)
+            VALUES (?, ?)
+            """, (cik, name)
+            )
+        self.cursor.execute("""
+            UPDATE institutes SET name = ?
+            WHERE cik = ?
+            """, (name, cik)
+            )
+        self.conn.commit()
